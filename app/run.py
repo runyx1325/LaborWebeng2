4efc5e5ca792cb2cc0d3078912cc24e03a85cf71
runyx1325 <lucaburst11@gmail.com>
2023-05-04 12:53:57 +0200

    dice

--- app/run.py
+++ app/run.py
@@ -151,7 +151,7 @@ def roll_dice(data):
     sid_cur_player = data['user']
     user_dict = json.loads(room_clients[room])
     nickname = json.dumps(user_dict["clients"].get(data['user']))
-    number = json.dumps(random.randrange(1,7))
+    number = json.dumps(random.randrange(5,7))
     game = room_game[room]
     game.set_cur_dice(0)
     game.set_cur_dice(number)
