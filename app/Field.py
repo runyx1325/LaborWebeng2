3ed856ca54b9413a4a3c531bc3e56138a2f9740b
tino <tino.daiss@gmail.com>
2023-05-18 11:49:58 +0200

    added comments to field

--- app/Field.py
+++ app/Field.py
@@ -13,19 +13,23 @@ class Field():
             self.color_on_field = 0
         self.figure_on_field = None
 
+    # return inner and outer color of the field
     def __str__(self):
         return str(self.color)+str(self.color_on_field)
 
+    # change inner color of the field to color of the new figure on the current field
     def set_figure_on_field(self, figure):
         self.figure_on_field = figure
         self.color_on_field = figure.get_color
         if figure.get_on_field != self:
             figure.set_on_field(self)
 
+    # remove figure from current field and change color to default
     def figure_away(self):
         self.figure_on_field = None
         self.color_on_field = 0
 
+    #getter
     @property
     def get_id(self):
         return self.id
