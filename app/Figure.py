82f3dfa68b234d68e3e01829ec747771d5264189
LeaGastgeb <inf21147@lehre.dhbw-stuttgart.de>
2023-05-18 12:07:14 +0200

    Kommentare hinzugefügt Figure

--- app/Figure.py
+++ app/Figure.py
@@ -1,13 +1,15 @@
 class Figure():
     def __init__(self, color, nr):
-        #nr := nr of figure (max 4)
-        #color := integer teamcolor for inner circle in fields of gameboard
-        #id := unmique id of figure in this game (team_color + figure_nr)
-        #home := true if figure is home
-        #finish := true if figure is in finish
-        #steps := 0 is home, 1 is starting field, 41/42/43/44 is finish
-        #on_field := field object
-        self.nr = nr 
+        # nr := nr of figure (max 4)
+        # color := integer teamcolor for inner circle in fields of gameboard
+        # id := unique id of figure in this game (team_color + figure_nr)
+        # starting_field :=
+        # home := true if figure is home
+        # finish := true if figure is in finish
+        # steps := 0 is home, 1 is starting field, 41/42/43/44 is finish
+        # on_field := field object
+
+        self.nr = nr
         self.color = color
         self.id = str(self.color)+str(self.nr)
         self.home_field = None
@@ -15,35 +17,43 @@ class Figure():
         self.finish_fields = None
         self.home = True
         self.finish = False
-        self.steps = 0 
+        self.steps = 0
         self.on_field = None
 
     def __str__(self):
         return self.id
 
+    # setter
+
     def set_on_field(self, field):
         self.on_field = field
-        
+
+        # walk if new field is not occupied by own figure
         if field.get_figure_on_field != self:
             field.set_figure_on_field(self)
 
     def set_home(self, field_dict):
+
         self.steps = 0
         self.home = True
         self.on_field = field_dict.get(int(self.id))
         self.get_on_field.set_figure_on_field(self)
 
     def walk(self, number):
+        # steps on own startingfiels
         if self.steps == 0:
             self.steps = 1
+        # walk figure
         else:
             self.steps += number
         self.home = False
+        # steps on finish_fields
         if self.steps > 40:
             self.finish = True
 
     def set_steps(self, number):
         self.steps = number
+        # steps on finish_fields
         if self.steps > 40:
             self.finish = True
 
@@ -56,33 +66,44 @@ class Figure():
     def set_finish_fields(self, fields):
         self.finish_fields = fields
 
+
+    # getter
     @property
     def get_nr(self):
         return self.nr
+
     @property
     def get_color(self):
         return self.color
+
     @property
     def get_id(self):
         return self.id
+
     @property
     def get_home_field(self):
         return self.home_field
+
     @property
     def get_starting_field(self):
         return self.starting_field
+
     @property
     def get_finish_fields(self):
         return self.finish_fields
+
     @property
     def get_home(self):
         return self.home
+
     @property
     def get_finish(self):
         return self.finish
+
     @property
     def get_steps(self):
         return self.steps
+
     @property
     def get_on_field(self):
         return self.on_field
